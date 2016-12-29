0afd0e0b7885a47f950a7ed719fea228a98c87d9
mattmartinez <mattmartinez816@gmail.com>
2016-12-28 21:00:42 -0800

    START BUTTON AND GAME OBJECT
    Added code for start button event and array of one suit of cards.

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   console.log('Ready to try your luck?');
 
 //Code for New Game Event
+var $startButton = $('newGame');
+$startButton.on("click", game.start);
 
 //Pop up window for Player name entry Event
 //Code to append player name to Bank Roll
@@ -10,3 +12,8 @@ $(document).ready(function() {
 });
 
 //This is the game object for Blackjack Attack
+var game = {
+//Array of deck of cards
+  deck: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'],
+  
+}
