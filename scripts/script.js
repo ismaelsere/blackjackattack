094670f2421c38c6ad4fdba4588a832a4485ed4f
mattmartinez <mattmartinez816@gmail.com>
2016-12-28 18:42:00 -0800

    JS NEW GAME MAPPING
    Outlined pseudocode in comments for New Game event.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log('Hello There!')
+//Once the game starts, player should see Title, New Game button, and empty board.
+$(document).ready(function() {
+  console.log('Ready to try your luck?');
+
+//Code for New Game Event
+
+//Pop up window for Player name entry Event
+//Code to append player name to Bank Roll
+
+});
+
+//This is the game object for Blackjack Attack
